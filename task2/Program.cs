de48e22ae90ffc83760c255e424175684611f114
Mikhail <M_I_X_A_I_L_1992@mail.ru>
2023-06-20 22:50:51 +0600

    finish

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number: ");
+
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number <= 99){
+        Console.WriteLine("Please enter a three-digit number");
+        return;
+    }
+
+    else {
+        // number = Convert.ToString(Console.ReadLine());
+string str = number.ToString();
+Console.WriteLine(str[2]);
+    }
