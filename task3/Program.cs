dedcd3e399dc7d661bf76abb7e1bb144f4f51409
Mikhail <M_I_X_A_I_L_1992@mail.ru>
2023-06-20 22:56:26 +0600

    finish

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a number from 1 to 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <=0 || number > 7 ){
+    Console.WriteLine("Enter a number from 1 to 7: ");
+    return;
+} 
+else if (number <= 5)
+    Console.WriteLine("This day is not a holiday");
+else if (number > 5)
+    Console.WriteLine("This day is a holiday");
