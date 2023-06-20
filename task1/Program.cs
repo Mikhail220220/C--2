52101dc788a36f5aeefe914ffbb74cbc25f7715d
Mikhail <M_I_X_A_I_L_1992@mail.ru>
2023-06-20 22:48:51 +0600

    finish

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 100 || number > 1000)
+        {
+            Console.WriteLine("Введите трех значное число");
+            return;
+        }
+        int result = number / 10 % 10;
+        
+            Console.WriteLine (result);
+        
+    }
+}
